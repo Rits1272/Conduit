b33c8d0ea74daccd502e8aaa3956d2a39e5a83a5
Ritik Jain <ritikjain1272@gmail.com>
2019-06-29 17:27:36 +0530

    Added Filtering functionaluty

--- conduit/apps/articles/urls.py
+++ conduit/apps/articles/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (ArticleViewSet, CommentsListCreateAPIView, CommentsDestroyAPIView, ArticlesFavoriteAPIView, TagListAPIView)
+from .views import (
+    ArticleViewSet, ArticlesFavoriteAPIView, ArticlesFeedAPIView,
+    CommentsListCreateAPIView, CommentsDestroyAPIView, TagListAPIView)
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'articles', ArticleViewSet)
 
 app_name='articles'
 urlpatterns = [
+    path('articles/feed', ArticlesFeedAPIView.as_view()),
 	path('', include(router.urls)),
 	path(
         'articles/<slug:article_slug>/comments',
@@ -20,4 +23,5 @@ urlpatterns = [
       ArticlesFavoriteAPIView.as_view()),
 
     path('tags', TagListAPIView.as_view()),
+    path('articles/feed', ArticlesFeedAPIView.as_view()),
 ]
--- conduit/apps/articles/views.py
+++ conduit/apps/articles/views.py
@@ -19,6 +19,25 @@ class ArticleViewSet(mixins.CreateModelMixin,
     renderer_classes = (ArticleJSONRenderer, )
     serializer_class = ArticleSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+
+        author = self.request.query_params.get('author', None)
+        if author is not None:
+            queryset = queryset.filter(author__user__username=author)
+
+        tag = self.request.query_params.get('tag', None)
+        if tag is not None:
+            queryset = queryset.filter(tags__tag=tag)
+
+        favorited_by = self.request.query_params.get('favorited', None)
+        if favorited_by is not None:
+            queryset = queryset.filter(
+                favorited_by__user__username=favorited_by
+            )
+
+        return queryset
+
 
     def create(self, request):
         serializer_context = {
@@ -62,7 +81,8 @@ class ArticleViewSet(mixins.CreateModelMixin,
 
     def list(self, request):
         serializer_context = {'request':request}
-        page = self.paginate_queryset(self.queryset)
+        page = self.paginate_queryset(self.get_queryset())
+
 
         serializer = self.serializer_class(
                 page, 
@@ -185,3 +205,24 @@ class TagListAPIView(generics.ListAPIView):
             'tags': serializer.data
         }, status=status.HTTP_200_OK)
 
+class ArticlesFeedAPIView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Article.objects.all()
+    renderer_classes = (ArticleJSONRenderer,)
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        return Article.objects.filter(
+            author__in=self.request.user.profile.follows.all()
+        )
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+
+        serializer_context = {'request': request}
+        serializer = self.serializer_class(
+            page, context=serializer_context, many=True
+        )
+
+        return self.get_paginated_response(serializer.data)
